e128ef7bcc9a0fea60703458358ea538c60186c1
WurirMacOs <wurir1994@gmail.com>
2026-04-27 23:28:20 +0200

    do 02

--- 02/app.js
+++ 02/app.js
@@ -1,12 +1,18 @@
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener('DOMContentLoaded', () => {
 
-function init(){
     const btnEl = document.querySelector('.cart__btn-calculate')
-    btnEl.addEventListener('click', handleClick)
-}
+    btnEl.addEventListener('click', () => {
+        const cartItemsElements = document.getElementsByClassName('cart__quantity')
+        const cartItemsList = [...cartItemsElements]
 
-function handleClick(){
-    const cartItemsList = document.getElementsByClassName('cart__item')
-    console.log(cartItemsList);
-    
-}
+        const totalPriceEl = document.querySelector('.cart__total-price')
+        let sum = 0
+
+        cartItemsList.forEach(element => {
+            const { value, dataset: { price } } = element
+
+            sum += Number(value) * Number(price)
+        })
+        totalPriceEl.textContent = sum
+    })
+})
