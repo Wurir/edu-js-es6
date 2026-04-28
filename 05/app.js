808cdc3fcc16867ed0141c92ae70af9270e7f6a7
WurirMacOs <wurir1994@gmail.com>
2026-04-28 23:33:15 +0200

    do 05

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
 
 const features = {
     skills: 10, 
@@ -14,3 +17,16 @@ const task = {
 const programmer = new Programmer( features );
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
 console.log(time);
+
+const junior1 = new Junior()
+const timeJunior = junior1.getApproximateTimeTaskDoneInHours(task)
+console.log(`Junior wykona task w czasie: ${timeJunior}`);
+
+const mid1 = new Mid()
+const timeMid = mid1.getApproximateTimeTaskDoneInHours(task)
+console.log(`Mid wykona task w czasie: ${timeMid}`);
+
+const senior1 = new Senior()
+const timeSenior = senior1.getApproximateTimeTaskDoneInHours(task)
+console.log(`Senior wykona task w czasie: ${timeSenior}`);
+
